1ac53fbd13fe52065c23c4fde2b5cc7ca25c0941
unknown <devkota9803@gmail.com>
2022-03-31 09:19:42 +0545

    Add to cart and checkout

--- medicines/models.py
+++ medicines/models.py
@@ -1,36 +1,41 @@
 import email
+from enum import Flag
 from pickle import TRUE
 from pickletools import read_uint1
 from pyexpat import model
 from statistics import mode
+from sys import maxsize
 from urllib.request import urlcleanup
 from xmlrpc.client import TRANSPORT_ERROR
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from jinja2 import ModuleLoader
+from numpy import maximum
 
 
 # Create your models here.
 
 
 
-Province=(
-    ('Province 1', 'Province 1'),
-    ('Province 2', 'Province 2'),
-    ('Province 3', 'Province 3'),
-    ('Province 4', 'Province 4'),
-    ('Province 5', 'Province 5'),
-    ('Province 6', 'Province 6'),
-    ('Province 7', 'Province 7'),
-)
+# Province=(
+#     ('Province 1', 'Province 1'),
+#     ('Province 2', 'Province 2'),
+#     ('Province 3', 'Province 3'),
+#     ('Province 4', 'Province 4'),
+#     ('Province 5', 'Province 5'),
+#     ('Province 6', 'Province 6'),
+#     ('Province 7', 'Province 7'),
+# )
 
-class Customer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200,null=True)
+# class Customer(models.Model):
+#     # user = models.OneToOneField(User,on_delete=models.CASCADE,null=True, blank=True)
+#     user = models.CharField(max_length=200,null=True)
+#     name = models.CharField(max_length=200, null=True)
+#     email = models.CharField(max_length=200,null=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class Product(models.Model):
     product_id = models.AutoField
@@ -55,13 +60,23 @@ class Product(models.Model):
         return url
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.CharField(max_length=20,null=True, blank=False)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping = False
+        # orderitems = self.orderitem_set.all()
+        # for i in orderitems:
+        #     if i.product_name.digital == False:
+        #         shipping = True
+       
+        return shipping
     
     @property
     def get_cart_total(self):
@@ -88,16 +103,16 @@ class OrderItem(models.Model):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    user = models.CharField(max_length=20,null=True, blank=False)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=100, null=True )
     city = models.CharField(max_length=100, null=True )
-    province = models.CharField(choices=Province, default='Province',max_length=20 )
+    ward_no = models.IntegerField(null=True)
     zip_code = models.CharField(max_length=50, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.address
+        return self.user
 
 
 
