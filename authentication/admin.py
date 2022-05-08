cffa80780a1d911fb50773264025956e2e831e91
unknown <devkota9803@gmail.com>
2022-05-08 11:33:02 +0545

    Django admin modiyf

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+admin.site.site_header = "Ausadhi Pasal"
+admin.site.site_title = 'Ausadhi Pasal'
+admin.site.index_title ="Ausadhi Pasal"
 # Register your models here.
